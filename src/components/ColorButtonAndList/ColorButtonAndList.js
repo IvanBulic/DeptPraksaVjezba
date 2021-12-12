f4e71fd83d27b825536f38118b311d9630217ed2
IBulic <ibulic01@fesb.hr>
2021-12-12 23:06:42 +0100

    Completed the bonus tasks - added input field with ability to add items on the list and to change color of button text - button changes text depending on the input field - cleaned the code (all unesessery code and console.log) - improved the look of the site - added the alert when user submits unvalid input

--- src/components/ColorButtonAndList/ColorButtonAndList.js
+++ src/components/ColorButtonAndList/ColorButtonAndList.js
@@ -1,7 +1,10 @@
 import React,{Component} from 'react';
 import ListElement from '../ListElement/ListElement';
 import './ColorButtonAndList.css';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+toast.configure();
 class ColorButton extends Component {
     constructor(props) {
         super(props);
@@ -9,9 +12,8 @@ class ColorButton extends Component {
             textColor : "",
             colorList: [],
             activeColorHex: "",
-            buttonText: "Enter color",
-            inputText: "aaa"
-        } 
+            buttonText: "Enter color"
+        }
     } 
 
     //Called when the button is clicked
@@ -25,28 +27,30 @@ class ColorButton extends Component {
             method:'GET',
             cache:'no-cache'
             };
-        //sets ip the new request
+        //sets up the new request
         var urlRequestNoCache = new Request("https://www.colr.org/json/color/random",init);
         
         //gets the new color from json file and stores it inside of the state textColor
         fetch(urlRequestNoCache).then(res => res.json()).then(
             (result) => {
                 newColor = result.new_color;
-                //checkes if the json file has any color value in it (if not function starts a recursive funcion and finds a new color and returns)
+                //checkes if the json file has any color value in it (if not function starts a recursive function and finds a new color and returns)
                 if (newColor===""){
-                    console.log("empty string, skip");
                     this.ButtonClicked();
                     return;
                 }
                 //sets state.textColor to a new value and stores the previous one
-                console.log(newColor);
                 previousColor = this.state.textColor;
-                //checkes if a color is already in the list, adds it if not, bolds it if it is
+                //checkes if a color is already in the list, adds it if not 
                 if(previousColor !=="" && this.state.colorList.find(element=>element===previousColor)===undefined){
-                    this.state.colorList.push(previousColor);
-                }else{
-                        console.log("postoji ista boja");
+                    //temp var used to change state with setState
+                    var temp = this.state.colorList;
+                    temp.push(previousColor);
+                    this.setState({
+                        colorList: temp
+                    })
                 }
+                //sets the button text color
                 this.setState({
                     textColor: "#" + newColor
                 });
@@ -55,26 +59,49 @@ class ColorButton extends Component {
         return;
     }
 
-    ArrowUpClick(color,index)
+    //On click event that handles up arrow control
+    ArrowUpClick(index)
     {
+        //return if the first element is clicked
         if(index === 0) {return;}
+        var temp;
+        var tempArray =this.state.colorList;
 
-        var temp = this.state.colorList[index];
-        this.state.colorList[index] = this.state.colorList[index - 1];
-        this.state.colorList[index - 1] = temp;
-        this.forceUpdate();
+        //switch array element
+        temp = tempArray[index];
+        tempArray[index] = tempArray[index - 1];
+        tempArray[index - 1] = temp;
+        
+        //set new colorList
+        this.setState({
+            colorList: tempArray
+        })
+
+        return;
     }
 
-    ArrowDownClick(color,index)
+    //On click event that handles down arrow control
+    ArrowDownClick(index)
     {
-        if(this.state.colorList.length <= (index + 1)) {console.log("nemoze dalje");return;}
+        //returns if the last element is clicked
+        if(this.state.colorList.length <= (index + 1)) {return;}
+        var temp;
+        var tempArray = this.state.colorList;
 
-        var temp = this.state.colorList[index];
-        this.state.colorList[index] = this.state.colorList[index + 1];
-        this.state.colorList[index + 1] = temp;
-        this.forceUpdate(); 
+        //switch array element
+        temp = this.state.colorList[index];
+        tempArray[index] = tempArray[index + 1];
+        tempArray[index + 1] = temp;
+
+         //set new colorList
+         this.setState({
+            colorList: tempArray
+        })
+
+        return;
     }
 
+    //Triggers every time user types something in the textfield
     InputTextChanged(event)
     {  
         this.setState({
@@ -82,21 +109,47 @@ class ColorButton extends Component {
         })
     }
 
+    //Triggers when user presses button in the textfield
     OnKeyPress(event){
+        //setup for the error notification
+        const ErrorNotification = () =>{
+            toast.error('Text input value not valid format (Example of a valid text: "#d0f66d")', {
+            position: "bottom-center",
+            autoClose: 5000,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            hideProgressBar: true,
+            });}
+
+        //sets up a regex for hexidecimal color
         var reg = /^#[0-9A-F]{6}$/i;
 
-        if(event.key === "Enter" && reg.test(this.state.buttonText))
+        //checks if key pressed is Enter, if not returns
+        if(event.key === "Enter")
         {
+            //checks if text in the input field is in valid format, if not pops up a notification
+            if (!reg.test(this.state.buttonText))
+            {
+                toast.dismiss(); 
+                ErrorNotification.apply();
+                return;
+            }
+
+            //adds new element to the list if there is no element with that value
             if (this.state.colorList.find(element=>element===this.state.buttonText) === undefined)
             {
-                this.state.colorList.push(this.state.buttonText)
+                var temp = this.state.colorList;
+                temp.push(this.state.buttonText)
+                this.setState({colorList:temp});       
             }
+            //sets button text color to user typed color hex
             this.setState({
                 textColor: this.state.buttonText
             })
-        }else{
-            console.log("nevalja")
         }
+        return;
     }
 
     render() { 
@@ -105,15 +158,15 @@ class ColorButton extends Component {
             <button disabled={this.state.isLoaded} style={{color: this.state.textColor}} className='Button' onClick={() => this.ButtonClicked()}>
                 {this.state.buttonText}
             </button>
-            <ul className='ColorList'>
+            <div className='ColorList'>
                 {this.state.colorList.map((color,index)=><div className='ListElementWithControls' key={color}><ListElement  color={color} activeColorHex={color} buttonTextColor={this.state.textColor}/>
                 &nbsp; &nbsp;
-                <div className='ArrowButton' onClick={()=>this.ArrowDownClick(color,index)}>&darr;</div>
+                <div className='ArrowButton' onClick={()=>this.ArrowDownClick(index)}>&darr;</div>
                 &nbsp; 
-                <div className='ArrowButton' onClick={()=>this.ArrowUpClick(color,index)}>&uarr;</div>
+                <div className='ArrowButton' onClick={()=>this.ArrowUpClick(index)}>&uarr;</div>
                 </div>)}
-            </ul>
-            <input type="text" name='ColorInput' defaultValue={this.state.buttonText} onChange={(event)=>this.InputTextChanged(event)} onKeyPress={(event)=>this.OnKeyPress(event)}/>
+            </div>
+            <input className='TextInput' type="text" name='ColorInput' defaultValue={this.state.buttonText} onChange={(event)=>this.InputTextChanged(event)} onKeyPress={(event)=>this.OnKeyPress(event)}/>
         </div>   
         );
     }
--- src/components/ListElement/ListElement.js
+++ src/components/ListElement/ListElement.js
@@ -10,11 +10,12 @@ class ListElement extends Component {
         }
     }
     render() { 
+        //sets text to bold if it button text color matches the color in the element
         var fontWeight = "normal"; 
         if (this.state.fontColor===this.props.buttonTextColor) {
             fontWeight= "bold"
         } 
-        return (<li className="MainElement" style={{color:this.props.color,fontWeight:fontWeight}} key={this.props.color}> {this.props.color} </li>);
+        return (<li className="MainElement" style={{color:this.props.color,fontWeight:fontWeight}} key={this.props.color}> <code>{this.props.color}</code> </li>);
     }
 }
  
